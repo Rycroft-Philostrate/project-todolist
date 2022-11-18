7349d4d5f416bb714760b05d15a056b3afe6bbb6
Rycroft-Philostrate <01_toni_stark@mail.ru>
2022-11-18 17:29:50 +0300

    settings

--- todolist/settings.py
+++ todolist/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
-        "NAME": os.environ.get("POSTGRES_NAME", "postgres"),
+        "NAME": os.environ.get("POSTGRES_BD", "postgres"),
         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
         "USER": os.environ.get("POSTGRES_USER", "postgres"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
