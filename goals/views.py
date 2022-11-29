e5b792b51fd0fc9058d0761fe27c4be91aa0995f
Rycroft-Philostrate <01_toni_stark@mail.ru>
2022-11-29 21:57:03 +0300

    Добавлен фукционал категорий, целей и комментариев

---
+++ goals/views.py
@@ -0,0 +1,126 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import permissions, filters
+from rest_framework.pagination import LimitOffsetPagination
+
+from goals.filters import GoalDateFilter
+from goals.models import GoalCategory, Goal, GoalComment
+from goals.serializers import GoalCreateSerializer, GoalCategorySerializer, GoalCategoryCreateSerializer, \
+    GoalSerializer, CommentCreateSerializer, CommentSerializer
+
+
+class GoalCategoryCreateView(CreateAPIView):
+    model = GoalCategory
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GoalCategoryCreateSerializer
+
+
+class GoalCategoryListView(ListAPIView):
+    model = GoalCategory
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GoalCategorySerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    ordering_fields = ["title", "created"]
+    ordering = ["title"]
+    search_fields = ["title"]
+
+    def get_queryset(self):
+        return GoalCategory.objects.filter(
+            user=self.request.user, is_deleted=False
+        )
+
+
+class GoalCategoryView(RetrieveUpdateDestroyAPIView):
+    model = GoalCategory
+    serializer_class = GoalCategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return GoalCategory.objects.filter(user=self.request.user, is_deleted=False)
+
+    def perform_destroy(self, instance):
+        instance.is_deleted = True
+        instance.save()
+        return instance
+
+
+class GoalCreateView(CreateAPIView):
+    model = Goal
+    serializer_class = GoalCreateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GoalView(RetrieveUpdateDestroyAPIView):
+    model = Goal
+    serializer_class = GoalSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Goal.objects.filter(
+            category__board__participants__user=self.request.user
+        )
+
+    def perform_destroy(self, instance):
+        instance.status = Goal.Status.archived
+        instance.save()
+        return instance
+
+
+class GoalListView(ListAPIView):
+    model = Goal
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GoalSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    filterset_class = GoalDateFilter
+    search_fields = ["title"]
+    ordering_fields = ["title", "created"]
+    ordering = ["title"]
+
+    def get_queryset(self):
+        return Goal.objects.filter(
+            category__board__participants__user=self.request.user
+        )
+
+
+class CommentCreateView(CreateAPIView):
+    model = GoalComment
+    serializer_class = CommentCreateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CommentView(RetrieveUpdateDestroyAPIView):
+    model = GoalComment
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return GoalComment.objects.filter(
+            goal__category__board__participants__user=self.request.user
+        )
+
+
+class CommentListView(ListAPIView):
+    model = GoalComment
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = LimitOffsetPagination
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ["goal"]
+    ordering = ["-created"]
+
+    def get_queryset(self):
+        return GoalComment.objects.filter(
+            goal__category__board__participants__user=self.request.user
+        )
